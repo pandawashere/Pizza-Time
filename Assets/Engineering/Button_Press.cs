8463fd9766170326dc92c62ec6d23cb80f927d1e
Alex Napolitan <cn025@hdm-stuttgart.de>
2017-11-14 21:46:32 -0500

    Made some scripts

---
+++ Assets/Engineering/Button_Press.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Press : MonoBehaviour {
+    public GameObject dough;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Engineering/Pizza_Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Pizza_Controller : MonoBehaviour {
+
+    public Pizza_Controller S;
+    public Material active;
+    public Material sauce;
+    public Material cheese;
+    public Material roni;
+    public Material peppers;
+    public Material mushrooms;
+    public Material bacon;
+    public Material dough;
+    public Material burnt;
+    public Material cooked;
+    //public GameObject trigger;
+
+
+	void Awake () {
+        S = this;
+
+    }
+
+	void Start () {
+        GetComponent<Renderer>().material = dough;
+        active = dough;
+    }
+	
+	
+	void Update () {
+        
+	}
+
+    void OnCollisionEnter(Collision col) {
+        GameObject other = col.gameObject;
+        switch (other.tag) {
+            case "sauce":
+                GetComponent<Renderer>().material = sauce;
+                active = sauce;
+                break;
+            case "cheese":
+                GetComponent<Renderer>().material = cheese;
+                active = cheese;
+                break;
+        }
+    }
+}
