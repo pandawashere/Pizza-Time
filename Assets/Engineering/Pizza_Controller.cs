333888bb7e2d21f4185a44049953af77e76e182c
Alex Napolitan <cn025@hdm-stuttgart.de>
2017-11-28 12:27:37 -0500

    WE COULD BE HEROES

--- Assets/Engineering/Pizza_Controller.cs
+++ Assets/Engineering/Pizza_Controller.cs
@@ -6,33 +6,51 @@ using Valve.VR;
 public class Pizza_Controller : MonoBehaviour {
 
     public Pizza_Controller S;
-    public Material active;
     public Material sauce;
     public Material cheese;
+    public Material sauceandcheese;
     public Material roni;
+    public Material scr;
+    public Material cr;
     public Material peppers;
+    public Material scp;
+    public Material scrp;
+    public Material cp;
+    public Material crp;
     public Material mushrooms;
+    public Material scm;
+    public Material scpm;
+    public Material scrpm;
+    public Material cpm;
+    public Material crpm;
     public Material bacon;
     public Material dough;
     public Material burnt;
     public Material cooked;
     public GameObject pizza;
+    public bool psauce = false;
+    public bool pcheese = false;
+    public bool proni = false;
+    public bool ppeppers = false;
+    public bool pmush = false;
+    public bool pbacon = false;
     //public Topping_Spawn tspawn;
 
-	void Awake () {
+    void Awake () {
         S = this;
         //tspawn = GetComponent<Topping_Spawn>();
     }
 
 	void Start () {
         pizza.GetComponent<Renderer>().material = dough;
-        //active = dough;
         //pizza = transform.Find("Pizza");
     }
 	
 	
 	void Update () {
-        
+        if (psauce == true && pcheese == true) {
+            pizza.GetComponent<Renderer>().material = sauceandcheese;
+        }
 	}
 
     void OnCollisionEnter(Collision col) {
@@ -42,14 +60,12 @@ public class Pizza_Controller : MonoBehaviour {
             case "sauce":
                 pizza.GetComponent<Renderer>().material = sauce;
                 Destroy(other);
-                //tspawn.S.spawn = 0;
-                //active = sauce;
+                psauce = true;
                 break;
             case "cheese":
                 pizza.GetComponent<Renderer>().material = cheese;
                 Destroy(other);
-                //tspawn.S.spawn = 0;
-                //active = cheese;
+                pcheese = true;
                 break;
         }
     }
