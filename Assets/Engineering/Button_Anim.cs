ffa24660db010621f7001e4a1d59ec3316cbc8cf
trilex14 <33504793+trilex14@users.noreply.github.com>
2017-12-08 15:14:39 -0500

    gfds
    gdf

--- Assets/Engineering/Button_Anim.cs
+++ Assets/Engineering/Button_Anim.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class Button_Anim : MonoBehaviour {
 
     Animation button;
+    public GameObject spawner;
+    public GameObject topping;
+    public GameObject buttonObject;
 
-	void Start () {
+    void Start () {
 		
 	}
 	
 	
 	void Update () {
-		
+		//if () {
+        //    buttonType();
+        //}
 	}
+
+    public void buttonType() {
+        if (buttonObject.tag == "tbutton") {
+            Instantiate(topping, spawner.transform.position, spawner.transform.rotation);
+        }
+        if (buttonObject.tag == "dbutton") {
+            Instantiate(topping, spawner.transform.position, spawner.transform.rotation);
+        }
+    }
 }
